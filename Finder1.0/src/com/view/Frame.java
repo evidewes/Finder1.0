cbccd06c62d55fd489891e675cd8edd50a487963
eduardo <eduardoancini@gmail.com>
2015-03-29 16:04:54 -0300

    escondendo files of type

--- Finder1.0/src/com/view/Frame.java
+++ Finder1.0/src/com/view/Frame.java
@@ -1,12 +1,16 @@
 package com.view;
 
+import java.awt.Component;
+import java.awt.Container;
 import java.awt.Dimension;
 import java.awt.EventQueue;
+import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.File;
 
 import javax.swing.JButton;
+import javax.swing.JComboBox;
 import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -20,56 +24,86 @@ public class Frame extends JFrame {
 
 	private static final long serialVersionUID = 1L;
 	private JTextField textField;
-	
+
 	public Frame() {
 		initComponents();
 	}
-	
+
 	private Controller controller = Controller.getInstance();
 
 	private void initComponents() {
 		// Configurações GERAIS
 		setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 		setCursor(new java.awt.Cursor(java.awt.Cursor.DEFAULT_CURSOR));
-		setSize(new Dimension(600,500));
+		setSize(new Dimension(600, 500));
 		setVisible(true);
 		getContentPane().setLayout(null);
-		
+
 		JLabel lblProcurarPor = new JLabel("Procurar por:");
 		lblProcurarPor.setBounds(51, 18, 104, 15);
 		getContentPane().add(lblProcurarPor);
-		
+
+		JLabel lblExemplo = new JLabel("exemplos: arquivo.txt;  arq;  txt...");
+		lblExemplo.setBounds(61, 36, 214, 15);
+		lblExemplo.setFont(new Font(lblExemplo.getFont().getName(), Font.ITALIC, lblExemplo.getFont().getSize()));
+		getContentPane().add(lblExemplo);
+
 		textField = new JTextField();
-		textField.setText("SAM_1706.JPG");
-		//textField.setText("paper");
+		// textField.setText("SAM_1706.JPG");
+		textField.setText("paper");
 		textField.setBounds(156, 16, 421, 19);
 		getContentPane().add(textField);
 		textField.setColumns(10);
-		
+
 		JFileChooser fileChooser = new JFileChooser();
 		fileChooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
-		fileChooser.setAcceptAllFileFilterUsed(false);
+		//fileChooser.setAcceptAllFileFilterUsed(false);
 		fileChooser.setBounds(61, 47, 526, 226);
 		fileChooser.setControlButtonsAreShown(false);
 		getContentPane().add(fileChooser);
-		
+
+		removeFileTypeComponents(fileChooser);
 		JList<String> list = new JList<String>();
 		list.setBounds(51, 327, 526, 133);
-		JScrollPane menuScrollPane  = new JScrollPane(list);
+		JScrollPane menuScrollPane = new JScrollPane(list);
 		menuScrollPane.setBounds(51, 327, 526, 133);
 		getContentPane().add(menuScrollPane);
-		
+
 		JButton btnNewButton = new JButton("Procurar");
 		btnNewButton.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {				
-				System.out.println(fileChooser.getSelectedFile().getName());
+			public void actionPerformed(ActionEvent e) {
+				if (fileChooser.getSelectedFile() == null) {
+					System.out.println("NI");
+					System.exit(0);
+				}
+
 				controller.search(fileChooser.getSelectedFile(), textField.getText(), list);
 			}
 		});
 		btnNewButton.setBounds(377, 265, 200, 50);
 		getContentPane().add(btnNewButton);
 	}
-	
+
+	void removeFileTypeComponents(Container con) {		
+		for (Component component : con.getComponents()) {
+			if (component instanceof JComboBox) {
+				Object sel = ((JComboBox<?>) component).getSelectedItem();
+				if (sel.toString().contains("AcceptAllFileFilter") || sel.toString().contains("versao em pt")) {
+					component.setVisible(false);
+				}
+			}
+			if (component instanceof JLabel) {
+				String text = ((JLabel) component).getText();
+				if (text.equals("Files of Type:") || text.equals("versao em pt:")) {
+					component.setVisible(false);					
+				}
+			}
+			if (component instanceof Container) {
+				removeFileTypeComponents((Container) component);
+			}
+		}
+	}
+
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
